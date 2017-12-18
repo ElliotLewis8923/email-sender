96a5eb737cf835b68d10abbf1d94ba2fe0b9ab84
Elliot Lewis <elewis8923@gmail.com>
2017-12-18 11:58:20 +1100

    remove sinon

--- server/util/format.spec.js
+++ server/util/format.spec.js
@@ -1,5 +1,4 @@
 const assert = require('assert')
-const sinon = require('sinon')
 const querystring = require('querystring')
 const format = require('./format')
 
--- server/util/validate.spec.js
+++ server/util/validate.spec.js
@@ -1,5 +1,4 @@
 const assert = require('assert')
-const sinon = require('sinon')
 const querystring = require('querystring')
 const validate = require('./validate')
 
